05316f1e81e8cbbe5024e93e16de635d2de554f1
Konstche <mymail@my.msail.ru>
2024-03-15 13:33:50 +0300

    rename file

--- prepare.sh
+++ prepare.sh
@@ -2,5 +2,5 @@ echo -n "Enter ip address: "
 read ip
 echo -n "port: "
 read port
-sed -i "s/.*Endpoint.*/Endpoint = $ip:$port/" ./full_config_generate.sh
+sed -i "s/.*Endpoint.*/Endpoint = $ip:$port/" ./full_user_config_generate.sh
 #
